f3d9fb1ceb71f4cf0c6b1a1faad08abdfcd61d1b
chloeeglouis <chloglouis@gmail.com>
2026-08-06 23:02:29 -0400

    organized code by creating methods and created a method to determine the instance of the data

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import json
 
-# prompt user for JSON file
-json_file = input("Enter JSON file name: ")
+# function that loads JSON file
+def load_json_file(file):
+    with open(file) as file:
+        data = json.load(file)
+    return data
 
-# open and load the content
-with open(json_file) as file:
-    data = json.load(file)
-print(data)
+# function that checks the instance of the data
+def instance_of_data(dt):
+    if isinstance(dt, dict):
+        print("This is a dictionary")
+    elif isinstance(dt, list):
+        print("This is a list")
+    else:
+        print("This is a plain value")
+
+# main method
+def main():
+    # get user input and load the file
+    json_data = input("Please enter json file path: ")
+    data = load_json_file(json_data)
+    print("Loaded json data:")
+
+    # determine the type of data
+    instance_of_data(data)
+
+if __name__ == '__main__':
+    main()
